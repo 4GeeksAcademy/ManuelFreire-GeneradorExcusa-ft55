b682cea42266a2ca2e9adc2a579f8b753800b2e5
ManuelFreire-rgb <mfreire.bernal@gmail.com>
2025-06-19 01:42:52 +0000

    se agrega funcion al proyecto

--- src/app.js
+++ src/app.js
@@ -4,21 +4,19 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-
   let who = ['The dog', 'My grandma', 'The mailman', 'My bird'];
   let action = ['ate', 'peed', 'crushed', 'broke'];
   let what = ['my homework', 'my phone', 'the car'];
   let when = ['before the class', 'when I was sleeping', 'while I was exercising', 'during my lunch', 'while I was praying'];
+  
+  function resultado(alojamiento) {
+    let resAleatorio = Math.floor(Math.random() * alojamiento.length)
+    return alojamiento[resAleatorio]
+  }
 
-  let randomExcuse = 
-    who[Math.floor(Math.random() * who.length)] + ' ' +
-    action[Math.floor(Math.random() * action.length)] + ' ' +
-    what[Math.floor(Math.random() * what.length)] + ' ' +
-    when[Math.floor(Math.random() * when.length)];
-
-  document.getElementById("excuse").innerHTML = randomExcuse;
+window.onload = function() {
+  //write your code here
+  let rendered = document.getElementById("excuse");
+  rendered.innerHTML = '<li>' + resultado(who) + ' ' + resultado(action) + ' ' + resultado(what) + ' ' + resultado(when) + '</li>';
 };
   
